44e159567338f549efca201d567e5a7ad070ed24
Ensiform <ensiform@gmail.com>
2011-09-18 22:49:20 -0500

    Derp clipboard sometimes fails

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 
-// For FS_CL_ExtractFromPakFile and FS_CL_ExtractFromPakFile:
+// For FS_CL_ExtractFromPakFile and FS_FOpenFileRead_Filtered:
 /*
 ===========================================================================
 
