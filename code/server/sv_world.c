1d1e1fefb04fab8ceb42537702d946c1a75f4812
Ensiform <ensiform@gmail.com>
2011-10-03 17:44:15 -0500

    Fixes missiles not colliding with each other when they should

--- code/server/sv_world.c
+++ code/server/sv_world.c
@@ -535,10 +535,10 @@ static void SV_ClipMoveToEntities( moveclip_t *clip ) {
 			if ( touchlist[i] == clip->passEntityNum ) {
 				continue;	// don't clip against the pass entity
 			}
-			if ( touch->r.ownerNum == clip->passEntityNum ) {
+			if ( !(touch->r.svFlags & SVF_OWNERNOTSHARED) && touch->r.ownerNum == clip->passEntityNum ) {
 				continue;	// don't clip against own missiles
 			}
-			if ( touch->r.ownerNum == passOwnerNum ) {
+			if ( !(touch->r.svFlags & SVF_OWNERNOTSHARED) && touch->r.ownerNum == passOwnerNum ) {
 				continue;	// don't clip against other missiles from our owner
 			}
 		}
