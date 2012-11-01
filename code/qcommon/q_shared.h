a9a2edf2ed4b3d5399e39c314fa0f1ab8c544a49
Ensiform <ensiform@gmail.com>
2012-11-01 01:48:07 -0500

    Only numbers are actually colors in JA plz kthx

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -402,7 +402,7 @@ extern	vec4_t		colorMdGrey;
 extern	vec4_t		colorDkGrey;
 
 #define Q_COLOR_ESCAPE	'^'
-#define Q_IsColorString(p)	((p) && *(p) == Q_COLOR_ESCAPE && *((p)+1) && isalnum(*((p)+1))) // ^[0-9a-zA-Z]
+#define Q_IsColorString(p)	((p) && *(p) == Q_COLOR_ESCAPE && *((p)+1) && isdigit(*((p)+1))) // ^[0-9]
 
 #define COLOR_BLACK	'0'
 #define COLOR_RED	'1'
