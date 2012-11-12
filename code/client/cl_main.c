99653f0d0eccbe8c6a9bba5a780de21d5cd248f7
Ensiform <ensiform@gmail.com>
2012-11-12 11:49:14 -0600

    Remove cl_anonymous

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -3586,7 +3586,6 @@ void CL_Init( void ) {
 	Cvar_Get ("handicap", "100", CVAR_USERINFO | CVAR_ARCHIVE );
 	Cvar_Get ("teamtask", "0", CVAR_USERINFO );
 	Cvar_Get ("sex", "male", CVAR_USERINFO | CVAR_ARCHIVE );
-	Cvar_Get ("cl_anonymous", "0", CVAR_USERINFO | CVAR_ARCHIVE );
 
 	Cvar_Get ("password", "", CVAR_USERINFO);
 	Cvar_Get ("cg_predictItems", "1", CVAR_USERINFO | CVAR_ARCHIVE );
