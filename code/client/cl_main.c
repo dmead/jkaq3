8b81888c9f71c60780e2734e40152dee70db76bb
Ensiform <ensiform@gmail.com>
2012-12-24 17:48:11 -0600

    Oops headmodel isn't in ja, stop trying to set it with /model (cmd)

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -3308,7 +3308,6 @@ void CL_SetModel_f( void ) {
 	arg = Cmd_Argv( 1 );
 	if (arg[0]) {
 		Cvar_Set( "model", arg );
-		Cvar_Set( "headmodel", arg );
 	} else {
 		Cvar_VariableStringBuffer( "model", name, sizeof(name) );
 		Com_Printf("model is set to %s\n", name);
